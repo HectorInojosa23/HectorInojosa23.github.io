3706f9319231d011ce25913084ec50c8551600f8
HectorInojosa23 <inojosahectorrr@gmail.com>
2022-11-12 16:58:53 -0400

    web 1.0.0

---
+++ JS.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+
+
+    const uno = document.getElementById('u');
+    uno.style.backgroundColor = 'blue';
+    uno.style.width = '0%';
+    var red, blue, green, alpha;
+    
+    
+
+});
