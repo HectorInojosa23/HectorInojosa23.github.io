0cdebcf63c9d3d5be2925459e4b7de7066d245f8
HectorInojosa23 <inojosahectorrr@gmail.com>
2022-11-13 22:35:40 -0400

    O 1.4

--- JS.js
+++ JS.js
@@ -1,8 +1,8 @@
-function changeImage() {
+function changebody() {
     var backbody = document.getElementsByTagName("body");
-    if (backbody.style.background.color == "#f3e2c7") {
-        backbody.style.background.color = "#f0f8ff";
+    if (backbody.style.backgroundColor === "#f3e2c7") {
+        backbody.style.backgroundColor = "#f0f8ff";
     } else {
-        backbody.style.background.color = "#f3e2c7";
+        backbody.style.backgroundColor = "#f3e2c7";
     }
   }
